3b41d7cbce949735c650cb286223b9c9175bbcb9
lchaps-mac <lukechapman182@gmail.com>
2025-12-30 23:10:25 +0000

    Changing calculate_standings() to conference_standings(). Incorporated nflseedR::nfl_standings() to help automate tiebreakers and seedings.

---
+++ R/conference_standings.R
@@ -0,0 +1,36 @@
+conference_standings <- function(
+    season,
+    rounds = "REG",
+    ranks = "CONF",
+    tiebreaker_depth = "SOS",
+    conference = NULL,
+    verbosity = "NONE"
+) {
+  
+  games <- load_schedule_data(
+    seasons = season,
+    rounds  = rounds
+  ) |>
+    dplyr::filter(
+      !is.na(home_score),
+      !is.na(away_score)
+    )
+  
+  standings <- nflseedR::nfl_standings(
+    games = games,
+    ranks = ranks,
+    tiebreaker_depth = tiebreaker_depth,
+    verbosity = verbosity
+  )
+  
+  if (!is.null(conference)) {
+    standings <- standings |>
+      dplyr::filter(conf == !!conference)
+  }
+  
+  standings |> 
+    dplyr::arrange(
+      conf_rank
+    )
+}
+
