7dc50743f6747ca0da8a2b0c1e8acdb77672250e
lchaps-mac <lukechapman182@gmail.com>
2025-12-30 19:54:38 +0000

    Changed file name to reflect the function name and add include_postseason function

--- R/load_pbp.R
+++
@@ -1,5 +0,0 @@
-load_pbp_data <- function(
-    seasons = 1999:2025
-) {
-  nflreadr::load_pbp(seasons)
-}
---
+++ R/load_pbp_data.R
@@ -0,0 +1,14 @@
+load_pbp_data <- function(
+    seasons = 1999:2025,
+    include_postseason = FALSE
+) {
+  pbp <- nflreadr::load_pbp(seasons)
+  
+  if(!include_postseason) {
+    pbp <- dplyr::filter(
+      pbp,
+      season_type == "REG"
+    )
+  }
+}
+
